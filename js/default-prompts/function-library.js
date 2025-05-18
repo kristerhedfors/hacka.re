c809d8f598fbae786e0c97ce1a0cd4495ec9168e
krister <krister.hedfors@gmail.com>
2025-05-18 08:55:12 +0200

    feat: add Function Library default prompt and integrate with Default Prompts Service

---
+++ js/default-prompts/function-library.js
@@ -0,0 +1,31 @@
+/**
+ * Function Library Default Prompt
+ * Provides all JavaScript functions in the Function Library as a default prompt
+ */
+
+window.FunctionLibraryPrompt = {
+    id: 'function_library',
+    name: 'All Javascript functions in Function Library',
+    content: function() {
+        // Get all functions from the Function Tools Service
+        const allFunctions = window.FunctionToolsService ? window.FunctionToolsService.getJsFunctions() : {};
+        
+        // If no functions, return a message
+        if (!allFunctions || Object.keys(allFunctions).length === 0) {
+            return 'No JavaScript functions are currently defined in the Function Library.';
+        }
+        
+        // Combine all function code
+        const combinedCode = Object.values(allFunctions)
+            .map(func => func.code)
+            .join('\n\n');
+        
+        return `# Function Library
+
+The following JavaScript functions are available in the Function Library:
+
+\`\`\`javascript
+${combinedCode}
+\`\`\``;
+    }
+};
